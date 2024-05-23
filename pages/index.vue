bab96588bbb49227ea4f2e19923ffcae79e9b6d4
Kukuh Satrio <70389916+satrio92@users.noreply.github.com>
2024-05-23 23:11:47 +0700

    init project

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+
+import {defineComponent} from "vue";
+import Button from "~/components/ui/button/Button.vue";
+
+export default defineComponent({
+  components: {Button}
+})
+</script>
+
+<template>
+ <div>
+   Index Page
+   <Button>Click Me</Button>
+ </div>
+</template>
+
+<style scoped>
+
+</style>
